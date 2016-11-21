8de3a6ccac71dcb4b67d5ddb718aa66b6fcf976b
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2016-11-21 20:27:48 +0200

    Added AllTipshopPokes.rar

---
+++ !sort.bat
@@ -0,0 +1,63 @@
+mkdir 123
+move 0*.* 123
+move 1*.* 123
+move 2*.* 123
+move 3*.* 123
+move 4*.* 123
+move 5*.* 123
+move 6*.* 123
+move 7*.* 123
+move 8*.* 123
+move 9*.* 123
+mkdir A
+move A*.* A
+mkdir B
+move B*.* B
+mkdir C
+move C*.* C
+mkdir D
+move D*.* D
+mkdir E
+move E*.* E
+mkdir F
+move F*.* F
+mkdir G
+move G*.* G
+mkdir H
+move H*.* H
+mkdir I
+move I*.* I
+mkdir J
+move J*.* J
+mkdir K
+move K*.* K
+mkdir L
+move L*.* L
+mkdir M
+move M*.* M
+mkdir N
+move N*.* N
+mkdir O
+move O*.* O
+mkdir P
+move P*.* P
+mkdir Q
+move Q*.* Q
+mkdir R
+move R*.* R
+mkdir S
+move S*.* S
+mkdir T
+move T*.* T
+mkdir U
+move U*.* U
+mkdir V
+move V*.* V
+mkdir W
+move W*.* W
+mkdir X
+move X*.* X
+mkdir Y
+move Y*.* Y
+mkdir Z
+move Z*.* Z
---
+++ !unsort.bat
@@ -0,0 +1,89 @@
+cd 123
+move 0*.* ..
+move 1*.* ..
+move 2*.* ..
+move 3*.* ..
+move 4*.* ..
+move 5*.* ..
+move 6*.* ..
+move 7*.* ..
+move 8*.* ..
+move 9*.* ..
+cd ..
+cd A
+move A*.* ..
+cd ..
+cd B
+move B*.* ..
+cd ..
+cd C
+move C*.* ..
+cd ..
+cd D
+move D*.* ..
+cd ..
+cd E
+move E*.* ..
+cd ..
+cd F
+move F*.* ..
+cd ..
+cd G
+move G*.* ..
+cd ..
+cd H
+move H*.* ..
+cd ..
+cd I
+move I*.* ..
+cd ..
+cd J
+move J*.* ..
+cd ..
+cd K
+move K*.* ..
+cd ..
+cd L
+move L*.* ..
+cd ..
+cd M
+move M*.* ..
+cd ..
+cd N
+move N*.* ..
+cd ..
+cd O
+move O*.* ..
+cd ..
+cd P
+move P*.* ..
+cd ..
+cd Q
+move Q*.* ..
+cd ..
+cd R
+move R*.* ..
+cd ..
+cd S
+move S*.* ..
+cd ..
+cd T
+move T*.* ..
+cd ..
+cd U
+move U*.* ..
+cd ..
+cd V
+move V*.* ..
+cd ..
+cd W
+move W*.* ..
+cd ..
+cd X
+move X*.* ..
+cd ..
+cd Y
+move Y*.* ..
+cd ..
+cd Z
+move Z*.* ..
